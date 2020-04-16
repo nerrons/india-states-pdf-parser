1379eefc52a53cc4d0a683d986a13ae3ef731dc4
nerrons <coniinedieu@gmail.com>
2020-04-16 21:02:08 +0800

    finally

--- date_extractor.py
+++ date_extractor.py
@@ -2,6 +2,9 @@ import pdftotext
 from pathlib import Path
 from pprint import pprint
 import dateparser
+from datetime import timedelta
+import shutil
+
 
 years_list = [ '2020-21', '2019-20', '2018-19', '2017-18', '2016-17', '2015-16', '2014-15', '2013-14' ]
 rawpdf_list = [ 'rawpdf' + y for y in years_list ]
@@ -21,12 +24,17 @@ for rp in rawpdf_list:
                 #date_text = text.split('Date of Reporting')[1].split('A. ')[0].replace(':', '').strip()
                 try:
                     date_text = text.split('Daily PSP Report for the date')[1].strip()[:10]
+                    source_second = False
                 except IndexError:
                     date_text = text.split('Date of Reporting')[1].split('A. ')[0].replace(':', '').strip()
-                date = dateparser.parse(date_text, date_formats=['%d.%m.%Y']).strftime("%Y-%m-%d")
-                #print(date)
+                    source_second = True
+                datetime_obj = dateparser.parse(date_text, date_formats=['%d.%m.%Y'])
+                if source_second:
+                    datetime_obj = datetime_obj - timedelta(days=1)
+                date = datetime_obj.strftime("%Y-%m-%d")
                 newpdf_name = f'{date}__{pdf_path.name}__.pdf'
-                pdf_path.replace(newpdf_dir / newpdf_name)
+                #pdf_path.replace(newpdf_dir / newpdf_name)
+                shutil.copy(str(pdf_path), str(newpdf_dir / newpdf_name))
 
             except Exception as e:
                 bad_list.append(pdf_path)
